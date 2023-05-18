3c3c2b42adda6628e3e7c21895e9941a4705d075
LisaPermyakova <elizaveta.permyakova.2017@gmail.com>
2023-05-18 17:53:26 +0300

    homework03

--- homework03/caesar.py
+++ homework03/caesar.py
@@ -8,27 +8,27 @@ ru_ABC = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
 
-    eng_A_caesar = ''
-    eng_a_caesar = ''
-    rus_A_caesar = ''
-    rus_a_caesar = ''
+    en_ABC_caesar = ''
+    en_abc_caesar = ''
+    ru_ABC_caesar = ''
+    ru_abc_caesar = ''
 
     for i in range(len(en_ABC)):
-        eng_A_caesar += en_ABC[(i + shift) % len(en_ABC)]
+        en_ABC_caesar += en_ABC[(i + shift) % len(en_ABC)]
     for i in range(len(en_abc)):
-        eng_a_caesar += en_abc[(i + shift) % len(en_abc)]
+        en_abc_caesar += en_abc[(i + shift) % len(en_abc)]
     for i in range(len(ru_ABC)):
-        rus_A_caesar += ru_ABC[(i + shift) % len(ru_ABC)]
+        ru_ABC_caesar += ru_ABC[(i + shift) % len(ru_ABC)]
     for i in range(len(ru_abc)):
-        rus_a_caesar += ru_abc[(i + shift) % len(ru_abc)]
+        ru_abc_caesar += ru_abc[(i + shift) % len(ru_abc)]
 
-    encrypt_eng_A = str.maketrans(en_ABC, eng_A_caesar)
-    encrypt_eng_a = str.maketrans(en_abc, eng_a_caesar)
-    encrypt_rus_A = str.maketrans(ru_ABC, rus_A_caesar)
-    encrypt_rus_a = str.maketrans(ru_abc, rus_a_caesar)
+    encrypt_en_ABC = str.maketrans(en_ABC, en_ABC_caesar)
+    encrypt_en_abc = str.maketrans(en_abc, en_abc_caesar)
+    encrypt_ru_ABC = str.maketrans(ru_ABC, ru_ABC_caesar)
+    encrypt_ru_abc = str.maketrans(ru_abc, ru_abc_caesar)
 
-    ciphertext = plaintext.translate(encrypt_eng_A).translate(encrypt_eng_a).translate(encrypt_rus_A).translate(
-        encrypt_rus_a)
+    ciphertext = plaintext.translate(encrypt_en_ABC).translate(encrypt_en_abc).translate(encrypt_ru_ABC).translate(
+        encrypt_ru_abc)
 
     return ciphertext
 
