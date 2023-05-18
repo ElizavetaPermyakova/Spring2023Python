aabd4a67dfbb491624ee1596678dbfdc5a91be52
LisaPermyakova <elizaveta.permyakova.2017@gmail.com>
2023-05-18 17:42:52 +0300

    homework03

---
+++ homework03/rsa.py
@@ -0,0 +1,90 @@
+import random
+import typing as tp
+
+
+def is_prime(n: int) -> bool:
+    """Tests to see if a number is prime."""
+    k = 0
+    for i in range(2, n // 2 + 1):
+        if (n % i == 0):
+            k = k + 1
+    if (k <= 0) and (n != 1): return True
+    elif (n == 1): return False
+    else: return False
+
+
+
+def gcd(a: int, b: int) -> int:
+    """Euclid's algorithm for determining the greatest common divisor."""
+    while (a != 0) and (b != 0):
+        if a >= b:
+            a %= b
+        else:
+            b %= a
+    return a or b
+
+
+
+def multiplicative_inverse(e: int, phi: int) -> int:
+    """Euclid's extended algorithm for finding the multiplicative
+    inverse of two numbers.
+    """
+    for x in range(1, phi):
+        if (e * x) % phi == 1:
+            return x
+    return 0
+
+
+def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
+    if not (is_prime(p) and is_prime(q)):
+        raise ValueError("Both numbers must be prime.")
+    elif p == q:
+        raise ValueError("p and q cannot be equal")
+
+    n = p*q
+    phi = (p-1)*(q-1)
+    e = random.randrange(1, phi)
+
+    g = gcd(e, phi)
+    while g != 1:
+        e = random.randrange(1, phi)
+        g = gcd(e, phi)
+
+    d = multiplicative_inverse(e, phi)
+
+    return ((e, n), (d, n))
+
+
+def encrypt(pk: tp.Tuple[int, int], plaintext: str) -> tp.List[int]:
+    # Unpack the key into it's components
+    key, n = pk
+    # Convert each letter in the plaintext to numbers based on
+    # the character using a^b mod m
+    cipher = [(ord(char) ** key) % n for char in plaintext]
+    # Return the array of bytes
+    return cipher
+
+
+def decrypt(pk: tp.Tuple[int, int], ciphertext: tp.List[int]) -> str:
+    # Unpack the key into its components
+    key, n = pk
+    # Generate the plaintext based on the ciphertext and key using a^b mod m
+    plain = [chr((char ** key) % n) for char in ciphertext]
+    # Return the array of bytes as a string
+    return "".join(plain)
+
+
+if __name__ == "__main__":
+    print("RSA Encrypter/ Decrypter")
+    p = int(input("Enter a prime number (17, 19, 23, etc): "))
+    q = int(input("Enter another prime number (Not one you entered above): "))
+    print("Generating your public/private keypairs now . . .")
+    public, private = generate_keypair(p, q)
+    print("Your public key is ", public, " and your private key is ", private)
+    message = input("Enter a message to encrypt with your private key: ")
+    encrypted_msg = encrypt(private, message)
+    print("Your encrypted message is: ")
+    print("".join(map(lambda x: str(x), encrypted_msg)))
+    print("Decrypting message with public key ", public, " . . .")
+    print("Your message is:")
+    print(decrypt(public, encrypted_msg))
---
+++ homework03/vigenere.py
@@ -0,0 +1,46 @@
+def encrypt_vigenere(plaintext: str, keyword: str) -> str:
+    """Encrypts plaintext using a Vigenere cipher."""
+    ciphertext = ''
+    keyword = keyword.lower()
+    a = 0
+    while len(keyword) < len(plaintext):
+        keyword += keyword[a : a + 1]
+        a += 1
+
+    for i in range(len(plaintext)):
+        limit = 122 #конец нижнего регистра
+        if plaintext[i].isupper():
+            limit = 90 #верхний регистр
+        if not plaintext[i].isalpha():
+            ciphertext += plaintext[i]
+            continue
+        if ord(plaintext[i]) + ord(keyword[i]) % 97 <= limit:
+            letter = chr(ord(plaintext[i]) + ord(keyword[i]) % 97)
+        else:
+            letter = chr((ord(plaintext[i]) + ord(keyword[i]) % 97) - 26)
+        ciphertext += letter
+    return ciphertext
+
+
+def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
+    """Decrypts a ciphertext using a Vigenere cipher."""
+    plaintext = ''
+    keyword = keyword.lower()
+    a = 0
+    while len(keyword) < len(ciphertext):
+        keyword += keyword[a : a + 1]
+        a += 1
+
+    for i in range(len(ciphertext)):
+        limit = 97 #начало нижнего регистра
+        if ciphertext[i].isupper():
+            limit = 65 #начало верхнего регистра
+        if not ciphertext[i].isalpha():
+            plaintext += ciphertext[i]
+            continue
+        if ord(ciphertext[i]) - ord(keyword[i]) % 97 >= limit:
+            letter = chr(ord(ciphertext[i]) - ord(keyword[i]) % 97)
+        else:
+            letter = chr((ord(ciphertext[i]) - ord(keyword[i]) % 97) + 26)
+        plaintext += letter
+    return plaintext
